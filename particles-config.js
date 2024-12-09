5a57983b5092b96388a03df72c39e148f35cd0dd
shubhamaryal <aryalshubham62@gmail.com>
2024-12-09 19:53:29 +0545

    website

---
+++ particles-config.js
@@ -0,0 +1,59 @@
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.5
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 6
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            push: {
+                particles_nb: 4
+            },
+            repulse: {
+                distance: 100,
+                duration: 0.4
+            }
+        }
+    },
+    retina_detect: true
+});
